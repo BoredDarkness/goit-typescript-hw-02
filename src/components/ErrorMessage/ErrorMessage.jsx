da3348701f78a71445e51a931cce205994f6c9a1
BoredDarkness <gwinnblaidd@gmail.com>
2025-04-27 13:24:17 +0200

    summary

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./ErrorMessage.module.css";
-
-export default function ErrorMessage({ message }) {
-  return <div className={styles.error}>{message}</div>;
-}
